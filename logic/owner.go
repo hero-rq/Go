4bc4622bd888081fb23a1eb6dde39132805e11d3
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-06-20 12:40:15 +0900

    Create owner.go

---
+++ logic/owner.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const panda string = "computer"
+
+func main() {
+	var age int = 25
+	name := panda
+  age = 13
+	fmt.Println(name)	
+  fmt.Println(panda)
+	fmt.Println(age)
+}
