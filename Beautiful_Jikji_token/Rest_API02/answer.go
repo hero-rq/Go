ce0938a4515c760dfa0e16b1bba6e2cf3bcfb03a
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-07-01 12:18:58 +0900

    Create answer.go

---
+++ Beautiful_Jikji_token/Rest_API02/answer.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/hero-rq/learngo/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
+
+func main() {
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on https://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
